810d937389af95c5552a9abc1511e06f8fb8a774
Andrea Cleland <andreaclelan@gmail.com>
2022-05-05 22:10:12 -0700

    not sure anything changed

--- src/services/fetch.js
+++ src/services/fetch.js
@@ -1,6 +1,5 @@
 export async function fetchCharacters() {
   const res = await fetch('https://rickandmortyapi.com/api/character');
-  console.log(res);
   const data = await res.json();
   return data.results;
 }
@@ -9,6 +8,5 @@ export async function fetchCharacter(id) {
   const res = await fetch(`https://rickandmortyapi.com/api/character/${id}`);
 
   const data = await res.json();
-  console.log(data);
   return data;
 }
